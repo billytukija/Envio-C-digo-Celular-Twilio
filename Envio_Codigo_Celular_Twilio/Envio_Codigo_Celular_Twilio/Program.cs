2d0d4ef0b60ffd0a3d136c3276e3f934be8507f8
Billy Tukija Richard <billy.tukija@onlineapp.com.br>
2019-11-29 18:46:27 -0200

    Simple project using twilio for send a smg to a phone number

---
+++ Envio_Codigo_Celular_Twilio/Envio_Codigo_Celular_Twilio/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Twilio;
+using Twilio.Exceptions;
+using Twilio.Rest.Api.V2010.Account;
+
+namespace Envio_Codigo_Celular_Twilio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                const string accountSid = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+                const string authToken = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+
+                TwilioClient.Init(accountSid, authToken);
+
+                var message = MessageResource.Create(
+                    body: "Código de confirmação 1591",
+                    from: new Twilio.Types.PhoneNumber("+55xxxxxxxxxxx"),
+                    to: new Twilio.Types.PhoneNumber("+55xxxxxxxxxxx")
+                );
+
+                Console.WriteLine(message.Sid);
+
+            }
+            catch (ApiException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine($"Twilio Error {e.Code} - {e.MoreInfo}");
+            }
+        }
+    }
+}
